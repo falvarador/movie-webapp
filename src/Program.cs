57dd2b1f6ef73789431514b119248a9a8ec3ad26
falvarado <alvaguez1990@gmail.com>
2020-05-08 22:25:01 -0600

    Creación de acciones CRUD para géneros y películas

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-
 namespace WebApp
 {
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Blazor.FileReader;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using MovieWeb.WebApp.Common;
+    using MovieWeb.WebApp.Services;
+
     public class Program
     {
         public static async Task Main(string[] args)
@@ -14,9 +16,17 @@ namespace WebApp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddBaseAddressHttpClient();
-
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            ConfigureServices(builder.Services);
+            
             await builder.Build().RunAsync();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IBaseService, BaseService>();
+            services.AddScoped<IShowMessage, ShowMessage>();
+            services.AddFileReaderService(options => options.InitializeOnFirstCall = true);
         }
     }
 }
